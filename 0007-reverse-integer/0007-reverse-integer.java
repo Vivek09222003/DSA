2f1be47341323d6438c3449809f213116aa79441
Vivek Chandra <151712264+Vivek09222003@users.noreply.github.com>
2026-09-14 22:42:19 +0530

    Time: 1 ms (99.97%), Space: 42.4 MB (90.39%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -3,21 +3,22 @@ class Solution {
         int temp = Math.abs(x);
         int count = 0;
 
-
         while(temp>0){
             temp = temp/10;
             count++;
         }
+        
         temp = Math.abs(x);
         long num = 0;
+
         while(temp>0){
             num += (temp%10) * (long)Math.pow(10,count-1);
             temp = temp/10;
             count--;
         }
-        if(x<0){
+
+        if(x<0)
             num = num * (-1);
-        }
 
         if(num<Integer.MIN_VALUE || num>Integer.MAX_VALUE)
             return 0;
